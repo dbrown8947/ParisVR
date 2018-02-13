fcb52c393494748646ac589be362b99afb9a7f03
Anthony <anthonyb33@live.ca>
2018-02-13 09:23:44 -0500

    Merge with ImportOSM
    This version has merged with the ImportOSM branch

--- Assets/Scripts/ImportDropDown.cs
+++ Assets/Scripts/ImportDropDown.cs
@@ -7,20 +7,19 @@ using UnityEngine.UI;
 public class ImportDropDown : MonoBehaviour {
 
 
-    public Dropdown dropdown;
-    public string filepath;
-    private List<string> objObjects = new List<string>();
+	public Dropdown dropdown;
+	public string filepath;
+	private List<string> objObjects = new List<string>();
 
-    // Use this for initialization
-    void Start()
-    {
-        string[] tempPath = Directory.GetFiles(@filepath, "*.obj");
+	// Use this for initialization
+	void Start()
+	{
+		string[] tempPath = Directory.GetFiles (@filepath, "*.obj");
 
-        for (int i = 0; i < tempPath.Length; i++)
-        {
-            objObjects.Add(tempPath[i]);
-        }
+		for (int i = 0; i < tempPath.Length; i++) {
+			objObjects.Add (tempPath [i]);
+		}
 
-        dropdown.AddOptions(objObjects);	
+		dropdown.AddOptions (objObjects);
 	}
 }
